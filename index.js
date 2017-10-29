f2bf13c8d61d8efea0e210f79e7eb545ef8a3b5d
MariaMatkowski <m.matkowski@gmx.de>
2017-10-29 13:38:51 +0100

    map getestet

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ const job = require('./job')
 const credit = require('./credit')
 
 
+var temps = [20,18,26,30]
 
+var newTemps = temps.map(function(tempF){
+    return Math.round (tempF * (9/5) +32);
+})
 
+console.log(temps);
+console.log(newTemps);
+
+/*
 const Traumwohnung = new flat("Mitte", "75", 250000);
 const alternativWohnung = new flat("Charlottenburg", "65", 260000);
 
@@ -21,3 +29,4 @@ while (count<=10) {
 }
 
 console.log(total);
+*/
