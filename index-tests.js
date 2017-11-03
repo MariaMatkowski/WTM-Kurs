8dc6553c6fb99f98b56bf4be76a4e09986974308
MariaMatkowski <m.matkowski@gmx.de>
2017-11-03 21:47:49 +0100

    switch und case - ähnlich if & else

--- index-tests.js
+++ index-tests.js
@@ -3,3 +3,18 @@ var b = 5;
 var grösser = (a>b)?a:b;
     document.write('a: ' + a + '<br>' + 'b: ' + b + '<br>');
     document.write('Die größere Zahl ist: ' + grösser);
+
+
+    
+switch(land) {
+        case "Italien":
+            alert("Hauptstadt: Rom");
+            break;
+        case "Wales":
+        case "England":
+        case "Schottland":
+            alert("Hauptstadt: London");
+            break;
+        default:
+            alert("Hauptstadt des Staates ist uns nicht bekannt");
+}
