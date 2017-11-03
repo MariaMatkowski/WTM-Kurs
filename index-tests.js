02c44ba7c065f334a37f9e55ed5fa0118d15756f
MariaMatkowski <m.matkowski@gmx.de>
2017-11-03 22:35:21 +0100

    added a table with for loop

--- index-tests.js
+++ index-tests.js
@@ -4,8 +4,8 @@ var grösser = (a>b)?a:b;
     document.write('a: ' + a + '<br>' + 'b: ' + b + '<br>');
     document.write('Die größere Zahl ist: ' + grösser);
 
+/*-----------------------------------------------------------------------*/
 
-    
 switch(land) {
         case "Italien":
             alert("Hauptstadt: Rom");
@@ -18,3 +18,27 @@ switch(land) {
         default:
             alert("Hauptstadt des Staates ist uns nicht bekannt");
 }
+
+/* ---------------------------------------------------------------------------*/
+
+<head>
+<style>
+    table, td {
+        border: 1px solid black
+    }
+</style>
+</head>
+
+<body>
+<table>
+    <script>
+        for(var zeile=1; zeile<=5; zeile++)
+        {
+            document.write('<tr>');
+            for(var spalte=1; spalte<=4; spalte++)
+                document.write('<td> Zeile' + zeile + '/Spalte'+spalte + '</td>');
+            document.write('</tr>');
+        }
+    </script>
+</table>
+</body>
