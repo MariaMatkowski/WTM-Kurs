2dee2dcb0ae050108649847a0a5b89ff1dcb228b
MariaMatkowski <m.matkowski@gmx.de>
2017-11-03 20:39:04 +0100

    if else

---
+++ index-tests.js
@@ -0,0 +1,5 @@
+var land = "Timbuktu";
+if (land == "Spanien")
+    document.write('<p>Land ist ' + land + '</p>');
+else (land != "Spanien")
+    document.write('<p>Land ist nicht Spanien, Land ist ' + land + '</p>');
